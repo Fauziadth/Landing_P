c548be7f1485a3ab656c4db0e6715df86c123c35
Fauziadth <fauzi.adithya@gmail.com>
2021-07-30 22:58:38 +0700

    Updates

---
+++ precache-manifest.e752c285db3874179f61e7a85d9c8752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd987d0a60425d38c253291e21ab81a9",
+    "url": "/Landing_P/index.html"
+  },
+  {
+    "revision": "6b339b06e9e78b902621",
+    "url": "/Landing_P/static/css/main.98ee0874.chunk.css"
+  },
+  {
+    "revision": "bbc118254ddbbc72193f",
+    "url": "/Landing_P/static/js/2.95055948.chunk.js"
+  },
+  {
+    "revision": "6b339b06e9e78b902621",
+    "url": "/Landing_P/static/js/main.9c4ecd0e.chunk.js"
+  },
+  {
+    "revision": "a30a7c461c2529bd7eb9",
+    "url": "/Landing_P/static/js/runtime~main.8f48f71a.js"
+  }
+]);
