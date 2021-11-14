16a353e466f61832f870495224974a93cad86a60
Fauziadth <zaynt16@gmail.com>
2021-11-14 22:54:15 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Landing_P/precache-manifest.07faa6084d8fc5623a0de424cef11018.js"
+  "/Landing_P/precache-manifest.02ec7d803010bd280cde988fe17a67f5.js"
 );
 
 self.addEventListener('message', (event) => {
