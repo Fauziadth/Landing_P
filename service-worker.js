011271bd3cce9ca930a65267d5e99ed31a1668f0
Fauziadth <zaynt16@gmail.com>
2021-11-14 22:22:40 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Landing_P/precache-manifest.29d507b60feb3cfcef3dc352d1e19fd0.js"
+  "/Landing_P/precache-manifest.c6e08c5da9191e67c0e298d37bf80094.js"
 );
 
 self.addEventListener('message', (event) => {
