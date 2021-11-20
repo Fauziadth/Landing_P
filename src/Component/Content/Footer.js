4b1e077a9df28573a816b571dbffd350bc246c6e
Fauziadth <zaynt16@gmail.com>
2021-11-20 22:18:43 +0700

    Footer and contact page content

--- src/Component/Content/Footer.js
+++ src/Component/Content/Footer.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
+const address = [
+      'Perumahan Jatinegara Baru',
+      'Jl. Gunung Taman Puspa AA 33',
+      'Penggilingan, Cakung, Jakarta Timur',
+      'Telp. (021) 2246 1256/76',
+      'Fax. (021) 2246 1207'
+]
+
 const Footer = () => {
   
   return (
-    <div className="footer_custom row justify-content-center align-items-center">
-        <div className="col-10">
+    <div className="footer_custom row justify-content-center">
+        <div className="col-8 row">
+          <h3>
+            Invitek Daya Industri
+          </h3>
+          <div className="col-6">
+            <p>
+              {`Specializing in delivering digitalization and renewable energy solutions in simplest way, Our team brings a diverse skill set with impressive experience provide simple solutions on digitalization and renewable energy to reach your sustainability.`}
+            </p>
+          </div>
+          <div className="col-1">
+          </div>
+          <div className="col-5">
+            {address.map( (det, idx) => 
+              <p key={idx}>
+                {det}
+              </p>
+            )}
+          </div>
         </div>
     </div>
   );
--- src/Component/Page/Contact.js
+++ src/Component/Page/Contact.js
@@ -2,15 +2,46 @@ import React, { Fragment } from 'react';
 import Footer from '../Content/Footer';
 import Wrapper from '../Part/Wrapper';
 
+const contactList = [{
+    name : 'Kantor Pusat',
+    details : [
+        'Perumahan Jatinegara Baru',
+        'Jl. Gunung Taman Puspa AA 33',
+        'Penggilingan, Cakung, Jakarta Timur',
+        'Telp. (021) 2246 1256/76',
+        'Fax. (021) 2246 1207'
+    ]
+},{
+    name : 'Kantor Cabang',
+    details : [
+        'SOHO Pancoran',
+        'MT Haryono St No. Kav 2-3, RT.1/RW.6',
+        'West Tebet,  Tebet, South Jakarta City',
+        'Jakarta 12810',
+        '',
+        'Jl. Kiarasari Permai IV No.2',
+        'Komplek Kiarasari Asri, Kec. Buah Batu',
+        'Kota Bandung, Jawa Barat 40286'
+    ]
+}]
+
 const Contact = () => {
     return (
         <Fragment>
             <Wrapper
-                subTitle={"You can reach us here"} title={"Our contact"} style={{ minHeight: '90vh', paddingTop: '30px' }}>
+                subTitle={"Our contacts"} title={"Get in touch with us"} style={{ minHeight: '90vh', paddingTop: '90px' }}>
                 <div className="row align-items-center">
 
-
-                    Under Construction
+                    {
+                        contactList.map(con => (
+                            <Fragment>
+                                <h1 style={{fontSize:'1.8em', marginTop : '40px'}}>{con.name}</h1>
+                                {con.details.map(det => (
+                                    det ? <h3 style={{lineHeight : '20px'}}>{det}</h3> : (<div style={{height : '10px'}}/>)
+                                ))}
+                            </Fragment>
+                        ))
+                    }
 
                     <div style={{ textAlign: 'center' }}>
 
